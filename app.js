80cf907f0fd5ac2117d5e453d4037b3d491850a5
hungrycat416 <muhiim_ali+cs1300@brown.edu>
2022-11-25 21:56:18 -0500

    Save

--- app.js
+++ app.js
@@ -27,13 +27,21 @@ const updatePage = async () => {
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
   // console.log(fruitsArray);
-
-  // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
-  // For example, find "name of all fruits whose sugar > 15",
-
+  // console.log(fruitsArray[1].nutritions.calories)
+  function filterfunction(obj){
+    return obj.nutritions.calories >= 100
+  }
+  const fruitnames = fruitsArray.filter(filterfunction)
+  function filterFunctwo(obj){
+    return obj.name
+  }
+  const nameArr = fruitnames.map(filterFunctwo)
   // TODO: Create a new HTML element to display your data
-
+  const newEl = document.createElement('div');
+  newEl.innerHTML = nameArr;
   // TODO: Append your new element to the page
+  const exEl = document.getElementById('fruits')
+  exEl.append(newEl);
 
 }
 
